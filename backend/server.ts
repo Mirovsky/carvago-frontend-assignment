c237b6277d3a4545042b7753e9d52bac32c7c894
Jakub Mirovský <mirovsky.jakub@gmail.com>
2025-12-30 13:28:54 +0100

    Fix cors

--- backend/server.ts
+++ backend/server.ts
@@ -14,7 +14,10 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 app.use(todoRoutes);
 app.use(userRoutes);
